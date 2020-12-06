6f558edd9a61e292a2bae9875bbd25b249c269e2
dainemarshall@gmail.com <dainemarshall@gmail.com>
2020-12-06 17:40:10 -0500

    working file upload to default /images directory

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const fileUpload = require('express-fileupload');
+// const multer = require('multer');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -20,11 +22,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(fileUpload());
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/email', emailRouter);
 app.use('/upload', uploadRouter);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/upload.js
+++ routes/upload.js
@@ -3,57 +3,80 @@ const router = express.Router();
 const fs = require('fs');
 const multer = require('multer');
 const path = require('path');
+const fileUpload = require('express-fileupload');
 
-const imageFilter = function(req, file, cb) {
-    // Accept images only
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = 'Only image files are allowed!';
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
-};
+
 
 router.get('/', (req, res) => {
     res.render('upload', { title: 'Upload' });
 })
 
 router.post('/', (req, res) => {
-    // define storage location
-    const storage = multer.diskStorage({
-        destination: function(req, file, cb) {
-            cb(null, `images/${req.body.folders}`);
-        },
+    console.log(req.files.image)
+    if (!req.files || Object.keys(req.files).length === 0) {
+        return res.status(400).send('No files were uploaded.');
+      }
     
-        // add file extension
-        filename: function(req, file, cb) {
-            cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        }
-    });
+      // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+      let image = req.files.image;
+    
+      // Use the mv() method to place the file somewhere on your server
+      image.mv(path.resolve(__dirname, `../public/images/${image.name}`), function(err) {
+        if (err)
+          return res.status(500).send(err);
+    
+        res.send('File uploaded!');
+      });
+});
 
-    let upload = multer({ storage: storage, fileFilter: imageFilter }).single('gallery_img');
 
-    upload(req, res, function(err) {
-        // req.file contains information of uploaded file
-        // req.body contains information of text fields, if there were any
 
-        if (req.fileValidationError) {
-            return res.send(req.fileValidationError);
-        }
-        else if (!req.file) {
-            return res.send('Please select an image to upload');
-        }
-        else if (err instanceof multer.MulterError) {
-            return res.send(err);
-        }
-        else if (err) {
-            return res.send(err);
-        }
 
-        // Display uploaded image for user validation
-        res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
-    });
+// router.post('/', (req, res) => {
+//     const imageFilter = function(req, file, cb) {
+//         // Accept images only
+//         if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+//             req.fileValidationError = 'Only image files are allowed!';
+//             return cb(new Error('Only image files are allowed!'), false);
+//         }
+//         cb(null, true);
+//     };
+    
+//     const storage = multer.diskStorage({
+//         destination: function(req, file, cb) {
+//             cb(null, `images/`);
+//         },
+      
+//         // add file extension
+//         filename: function(req, file, cb) {
+//             cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+//         }
+//       });
 
-})
+//     let upload = multer({ storage: storage, fileFilter: imageFilter }).single('gallery_img');
+
+//     upload(req, res, function(err) {
+//         // req.file contains information of uploaded file
+//         // req.body contains information of text fields, if there were any
+
+//         if (req.fileValidationError) {
+//             return res.send(req.fileValidationError);
+//         }
+//         else if (!req.file) {
+//             return res.send('Please select an image to upload');
+//         }
+//         else if (err instanceof multer.MulterError) {
+//             return res.send(err);
+//         }
+//         else if (err) {
+//             return res.send(err);
+//         }
+
+//         // Display uploaded image for user validation
+//         res.send(`You have uploaded this image: <hr/><img src="${req.file.path}" width="500"><hr /><a href="./">Upload another image</a>`);
+//     });
+
+// })
 
 
 
