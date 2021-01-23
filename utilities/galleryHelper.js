b899bb66c6fb5211cbba52bdac2293c45487650b
Daine Marshall <dmarshall@thelightsource.com>
2021-01-23 14:51:17 -0500

    basic layout css on index page

---
+++ utilities/galleryHelper.js
@@ -0,0 +1,9 @@
+
+/**
+ * This function takes in a gallery name string and gives back an array 
+ * of image names in the gallery
+ * @param {string} gName 
+ */
+function getGalleryImages(gName){
+
+}
