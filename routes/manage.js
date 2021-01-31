1914941db0e3718fde41ac1c6624d2368794815f
Daine Marshall <dmarshall@thelightsource.com>
2021-01-31 16:49:16 -0500

    specified nodejs version heroku

--- routes/manage.js
+++ routes/manage.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const fs = require('fs');
 // const multer = require('multer');
-//const path = require('path');
+const path = require('path');
 // const fileUpload = require('express-fileupload');
 
 const filePath = '../public/javascripts/gallery'
